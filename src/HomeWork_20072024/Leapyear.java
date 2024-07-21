05a8d5f7d7f7401b584a8f69a4305f6b77d2023b
TanyaSingh-QA <tanya.singhg08@gmail.com>
2024-07-21 15:55:47 +0530

    Added 21st july homework

--- src/HomeWork_20072024/Leapyear.java
+++ src/HomeWork_20072024/Leapyear.java
@@ -7,7 +7,7 @@ public class Leapyear {
         Scanner s = new Scanner(System.in);
         System.out.println("Enter the year you want to check");
         int year = s.nextInt();
-        if (year % 4 == 0) {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
             System.out.println("The given year is Leap Year");
         }
         else {
